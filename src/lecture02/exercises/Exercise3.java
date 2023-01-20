a5596a2937dd6074ddb719504edd85fda05671e7
lsavova <lsavova@vmware.com>
2023-01-20 15:04:56 +0200

    Lecture 2 - Intro to Java. Working with IntelliJ. Datatypes and variables

---
+++ src/lecture02/exercises/Exercise3.java
@@ -0,0 +1,12 @@
+package lecture02.exercises;
+
+public class Exercise3 {
+    // Print the area of a *rectangular triangle by given sides. Use formula to calculate it.
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int perimeter = (a * b) / 2;
+        System.out.println("Area is: " + perimeter);
+    }
+}
