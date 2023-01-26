e55d186997a03dd271c97bbed53b8a328fa55eae
lsavova <lsavova@vmware.com>
2023-01-26 10:47:45 +0200

    Added examples from lecture 4: Operators, expression, conditional statements and loops in Java

---
+++ src/lecture4/examples/loops/ContinueStatement.java
@@ -0,0 +1,12 @@
+package lecture4.examples.loops;
+
+public class ContinueStatement {
+    public static void main(String[] args) {
+        for (int j = 0; j <= 6; j++) {
+            if (j == 4) {
+                continue;
+            }
+            System.out.print(j + " ");
+        }
+    }
+}
