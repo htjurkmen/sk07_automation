c1971da288e686756a0065eca92930a301b37653
lvsav <lvsavova93@gmail.com>
2023-02-22 22:03:29 +0200

    Added examples from lecture 12 - Webdriver first steps

--- automation/src/test/java/lecture12/examples/WebDriverWithDriverPath.java
+++ automation/src/test/java/lecture12/examples/WebDriverWithDriverPath.java
@@ -1,12 +1,12 @@
 package lecture12.examples;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverWithDriverPath {
     public static void main(String[] args) {
+        // To run this example download the chromedriver.exe for your browser version
+        // and modify the path to it. This example works if the driver is put in automation/src/test/resources/drivers/ folder
         System.setProperty("webdriver.chrome.driver", "automation/src/test/resources/drivers/chromedriver.exe");
-        // WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
 
         driver.get("http://training.skillo-bg.com:4300/posts/all");
